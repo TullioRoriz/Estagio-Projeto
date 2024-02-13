4214936ab2ddeeab365e66376a5474c3bf2a4d97
Túllio Roriz Barbosa <133914030+TullioRoriz@users.noreply.github.com>
2024-02-13 09:19:18 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+let botao = document.querySelector("#botao");
+let pf = document.querySelector("#pf");
+
+botao.addEventListener('click', function(e){
+    pf.textContent = "As minhas expectativas são, aprender a como programar na prática e melhorar as minhas capacidades como programador, agregar a equipe, melhorar como profissional e pessoa.";
+    pf.style.animation = "paragrafo 7s";
+});
